33baddd909af62ca9b2b0cf5124f38b34b6637a7
David Christiansen <dave@trooptrack.com>
2021-11-25 12:03:34 -0700

    Potorazzi

--- contracts/Potorazzi.sol
+++ contracts/Potorazzi.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.0;
 import "./ERC721Tradable.sol";
 
 /**
- * @title Creature
- * Creature - a contract for my non-fungible creatures.
+ * @title Potorazzi
+ * Potorazzi - a contract for my non-fungible Potorazzis.
  */
-contract Creature is ERC721Tradable {
+contract Potorazzi is ERC721Tradable {
     constructor(address _proxyRegistryAddress)
-        ERC721Tradable("Creature", "OSC", _proxyRegistryAddress)
+        ERC721Tradable("Potorazzi", "PRZI", _proxyRegistryAddress)
     {}
 
     function baseTokenURI() override public pure returns (string memory) {
